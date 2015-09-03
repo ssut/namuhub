c1fa71a64f4a14bb918c3e21aee32877f917f138
ssut <ssut@ssut.me>
2015-09-03 16:40:30 +0900

    Add form submit event that replaces current page's uri

--- namuhub/static/namuhub.js
+++ namuhub/static/namuhub.js
@@ -1,15 +1,36 @@
+var ContribBox = React.createClass({
+    render: function() {
+        return <div />;
+    }
+});
+
 var SearchBox = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
+        console.log(this.props);
         return {
-            user: this.props.user
+            user: this.props.user || ''
         };
+    },
+
+    submit: function(e) {
+        var uri = '/' + this.state.user;
+        e.preventDefault();
+
+        var ps = window.history.pushState | 1 || 0;
+        [function(){location.replace(uri)},function(){window.history.pushState(null,null,uri)}][ps]();
+    },
+
+    updateUser: function(e) {
+        this.setState({
+            user: e.target.value
+        });
     },
 
     render: function() {
         return (
-            <form className="ui">
+            <form className="ui" onSubmit={this.submit}>
                 <div className="ui action center aligned input">
-                    <input type="text" placeholder="나무위키 아이디 입력" value={this.state.user} />
+                    <input type="text" placeholder="나무위키 아이디 입력" defaultValue={this.props.user} onChange={this.updateUser} />
                     <button className="ui teal button">조회</button>
                 </div>
             </form>
