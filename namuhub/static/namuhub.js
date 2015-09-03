a3bddbaddac3ed69c855108bec87b81efdc8fd5f
ssut <ssut@ssut.me>
2015-09-03 18:35:10 +0900

    Detect property changes and update component to be continuously

--- namuhub/static/namuhub.js
+++ namuhub/static/namuhub.js
@@ -3,8 +3,14 @@ var ContribBox = React.createClass({
         alert(user);
     },
 
+    componentWillReceiveProps: function(props) {
+        if(props.data !== this.props.data) {
+            // alert('yay! changed!');
+        }
+    },
+
     render: function() {
-        return <div>asdf</div>;
+        return <div>{this.props.data}</div>;
     }
 });
 
@@ -19,6 +25,11 @@ var SearchBox = React.createClass({
         var uri = '/' + this.state.user;
         e.preventDefault();
 
+        if(this.props.loading) {
+            alert('이미 불러오는 중입니다.');
+            return;
+        }
+
         var ps = window.history.pushState ? 1 : 0;
         [function(){location.replace(uri)},function(){window.history.pushState(null,null,uri)}][ps]();
 
@@ -32,15 +43,22 @@ var SearchBox = React.createClass({
     },
 
     componentDidMount: function() {
-        this.props.onSubmit(this.state.user);
+        if(this.state.user) {
+            this.props.onSubmit(this.state.user);
+        }
     },
 
     render: function() {
+        var btnClassString = 'ui teal button ';
+        if(this.props.loading) {
+            btnClassString += 'loading';
+        }
+
         return (
             <form className="ui" onSubmit={this.submit}>
                 <div className="ui action center aligned input">
-                    <input type="text" placeholder="나무위키 아이디 입력" defaultValue={this.props.user} onChange={this.updateUser} />
-                    <button className="ui teal button">조회</button>
+                    <input type="text" disabled={this.props.loading} placeholder="나무위키 아이디 입력" defaultValue={this.props.user} onChange={this.updateUser} />
+                    <button className={btnClassString}>조회</button>
                 </div>
             </form>
         );
